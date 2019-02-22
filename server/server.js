8da50ae483938916c7420afd467c632eb3599f11
Julio Cesar <darkent4ever@gmail.com>
2019-02-21 19:58:28 -0500

    COLOCANDO URI DIRECTO DE ATLAS, NO JALA EL ENV_NODE

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(require('./routes/usuario'))
 
-mongoose.connect(process.env.urlBD,(err)=>{
+mongoose.connect('mongodb://Darkent:wLipw3lQguDdzQPV@cluster0-shard-00-00-xwb72.mongodb.net:27017,cluster0-shard-00-01-xwb72.mongodb.net:27017,cluster0-shard-00-02-xwb72.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',(err)=>{
     if(!err) console.log('DB conectada');
 });
 
