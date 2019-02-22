c4196f48906077201eabc924daa18bd7197e8374
Julio Cesar <darkent4ever@gmail.com>
2019-02-21 20:39:44 -0500

    SE CREO UNA NUEBA BD CON EL IP 0.0.0.0

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV === 'dev'){
         uri = 'mongodb://localhost/cafe';
 }
 else{
-        uri = 'mongodb://Darkent:wLipw3lQguDdzQPV@cluster0-shard-00-00-xwb72.mongodb.net:27017,cluster0-shard-00-01-xwb72.mongodb.net:27017,cluster0-shard-00-02-xwb72.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+        uri = 'mongodb+srv://Darkent:0sXZErwGritLLCH9@cluster0-az1b1.mongodb.net/test?retryWrites=true';
 }
 
 process.env.urlBD = uri;
